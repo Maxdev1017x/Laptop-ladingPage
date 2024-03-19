513ca8caa87ba7059913ca3eda31e9ef4bffd86a
Maxdev1017x <Thelastmc1@gmail.com>
2024-03-19 11:55:10 -0300

    🛠️ Mudando a imagem para o html e add novo css correspondente

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,14 @@
 const buttons = {
-  "midnight": "url(src/image/macbook-air-midnight.jfif)",
-  "starlight": "url(src/image/macbook-air-starlight.jfif)",
-  "spacegray": "url(src/image/macbook-air-spacegray.jfif)",
-  "silver": "url(src/image/macbook-air-silver.jfif)"
+  "midnight": "src/image/macbook-air-midnight.jfif",
+  "starlight": "src/image/macbook-air-starlight.jfif",
+  "spacegray": "src/image/macbook-air-spacegray.jfif",
+  "silver": "src/image/macbook-air-silver.jfif"
 };
 
-let apple = document.getElementById("apple");
+let apple = document.querySelector(".background-image img");
 
 for (let button in buttons) {
   document.getElementById(button).addEventListener('click', function() {
-    apple.style.backgroundImage = buttons[button];
+    apple.src = buttons[button];
   });
 }
